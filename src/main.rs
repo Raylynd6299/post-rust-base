af1c8fdf7eb31102b8169fb9357ac4a735ce35f3
Ray Pulido <ray_escom@outlook.com>
2024-12-14 20:04:14 -0600

    first test of use ORM

--- src/main.rs
+++ src/main.rs
@@ -17,14 +17,34 @@ fn main() {
 
     let mut conn = PgConnection::establish(&db_url).expect("CONNECTION INPOSIBLE");
 
-    use self::models::Post;
+    use self::models::{Post, NewPost, PostSimple};
+    use self::schema::posts;
     use self::schema::posts::dsl::*;
 
+    // let new_post = NewPost{
+    //     title: "Mi segundo blog",
+    //     body: "lorem impsu",
+    //     slug: "primer_post"
+    // };
+
+    // let post_res:Post = diesel::insert_into(posts::table).values(&new_post).get_result(&mut conn).expect("Error insertando datos");
+
     //SELECT
+    // let posts_result = posts.load::<Post>(&mut conn).expect("Error en Query");
+
+    // let posts_result = posts.order(id.desc()).select((title,body)).limit(1).load::<PostSimple>(&mut conn).expect("Error en Query");
+
+    // let posts_result = posts.filter(id.eq(2)).limit(1).load::<Post>(&mut conn).expect("Error en Query");
+
+    // Update
+    // let post_update = diesel::update(posts.filter(id.eq(2))).set((title.eq("Tercer blog"),slug.eq("New_slug"))).get_result::<Post>(&mut conn).expect("Error en Query");
 
     let posts_result = posts.load::<Post>(&mut conn).expect("Error en Query");
 
+    //DElete
+    diesel::delete(posts.filter(slug.like("%post%"))).execute(&mut conn).expect("Fallo la eliminacion");
+
     for post in posts_result{
-        println!("{:?}",post.title);
+        println!("{:?} {}",post.title, post.slug);
     }
 }
