eeaa0eb6a8a59fbf4dc71452242d965acf8bb011
Ray Pulido <ray_escom@outlook.com>
2024-12-27 20:52:35 -0600

    feat: add endpoints

--- src/main.rs
+++ src/main.rs
@@ -8,43 +8,85 @@ use dotenv::dotenv;
 use std::env;
 use diesel::prelude::*;
 use diesel::pg::PgConnection;
+use diesel::r2d2::{ Pool, self, ConnectionManager };
 
-fn main() {
-    // READ .env file
+use actix_web::{ get, post, web, App, HttpResponse, HttpServer, Responder };
+
+use self::models::{ Post, NewPost, PostSimple, NewPostHandler };
+use self::schema::posts;
+use self::schema::posts::dsl::*;
+
+pub type DBPool = r2d2::Pool<ConnectionManager<PgConnection>>;
+
+#[get("/")]
+async fn index(pool: web::Data<DBPool>) -> impl Responder {
+    let mut conn = pool.get().expect("Error getting connectiong of db pool");
+
+    match web::block(move || { posts.load::<Post>(&mut conn) }).await {
+        Ok(data) => { HttpResponse::Ok().body(format!("{:?}\n", data)) }
+        Err(err) => HttpResponse::Ok().body(format!("Error: {:?}\n", err)),
+    }
+}
+
+#[post("/new_post")]
+async fn new_post(pool: web::Data<DBPool>, item: web::Json<NewPostHandler>) -> impl Responder {
+    let mut conn = pool.get().expect("Error getting connectiong of db pool");
+
+    match
+        web::block(move || {
+            Post::create_post(&mut conn,&item)
+        }).await
+    {
+        Ok(data) => { HttpResponse::Ok().body(format!("{:?}\n", data)) }
+        Err(err) => HttpResponse::Ok().body(format!("Error: {:?}\n", err)),
+    }
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
     dotenv().ok();
-
     let db_url = env::var("DATABASE_URL").expect("DB URL NOT FOUND!");
+    // let mut conn = PgConnection::establish(&db_url).expect("CONNECTION INPOSIBLE");
 
-    let mut conn = PgConnection::establish(&db_url).expect("CONNECTION INPOSIBLE");
+    let connection = ConnectionManager::<PgConnection>::new(db_url);
 
-    use self::models::{Post, NewPost, PostSimple};
-    use self::schema::posts;
-    use self::schema::posts::dsl::*;
+    let pool = Pool::builder().build(connection).expect("Error creating pool");
 
-    // let new_post = NewPost{
-    //     title: "Mi segundo blog",
-    //     body: "lorem impsu",
-    //     slug: "primer_post"
-    // };
+    // let post_res: Post = diesel
+    //     ::insert_into(posts::table)
+    //     .values(&new_post)
+    //     .get_result(&mut conn)
+    //     .expect("Error insertando datos");
 
-    // let post_res:Post = diesel::insert_into(posts::table).values(&new_post).get_result(&mut conn).expect("Error insertando datos");
-
-    //SELECT
     // let posts_result = posts.load::<Post>(&mut conn).expect("Error en Query");
 
-    // let posts_result = posts.order(id.desc()).select((title,body)).limit(1).load::<PostSimple>(&mut conn).expect("Error en Query");
+    // println!("Test ");
+    // for post in posts_result {
+    //     println!("{:?} {}", post.title, post.slug);
+    // }
 
-    // let posts_result = posts.filter(id.eq(2)).limit(1).load::<Post>(&mut conn).expect("Error en Query");
+    HttpServer::new(move || {
+        App::new().app_data(web::Data::new(pool.clone())).service(index).service(new_post)
+    })
+        .bind(("0.0.0.0", 9000))?
+        .run().await
 
-    // Update
-    // let post_update = diesel::update(posts.filter(id.eq(2))).set((title.eq("Tercer blog"),slug.eq("New_slug"))).get_result::<Post>(&mut conn).expect("Error en Query");
+    // //SELECT
+    // // let posts_result = posts.load::<Post>(&mut conn).expect("Error en Query");
 
-    let posts_result = posts.load::<Post>(&mut conn).expect("Error en Query");
+    // // let posts_result = posts.order(id.desc()).select((title,body)).limit(1).load::<PostSimple>(&mut conn).expect("Error en Query");
 
-    //DElete
-    diesel::delete(posts.filter(slug.like("%post%"))).execute(&mut conn).expect("Fallo la eliminacion");
+    // // let posts_result = posts.filter(id.eq(2)).limit(1).load::<Post>(&mut conn).expect("Error en Query");
 
-    for post in posts_result{
-        println!("{:?} {}",post.title, post.slug);
-    }
+    // // Update
+    // // let post_update = diesel::update(posts.filter(id.eq(2))).set((title.eq("Tercer blog"),slug.eq("New_slug"))).get_result::<Post>(&mut conn).expect("Error en Query");
+
+    // let posts_result = posts.load::<Post>(&mut conn).expect("Error en Query");
+
+    // //DElete
+    // diesel::delete(posts.filter(slug.like("%post%"))).execute(&mut conn).expect("Fallo la eliminacion");
+
+    // for post in posts_result{
+    //     println!("{:?} {}",post.title, post.slug);
+    // }
 }
--- src/models.rs
+++ src/models.rs
@@ -1,10 +1,16 @@
-#[derive(Queryable, Debug)]
+use super::schema::posts;
+use serde::{ Serialize, Deserialize };
+use diesel::prelude::*;
+use diesel::pg::PgConnection;
+use actix_web::{ web, HttpResponse };
+
+#[derive(Queryable, Debug, Deserialize, Serialize)]
 pub struct PostSimple {
     pub title: String,
     pub slug: String,
 }
 
-#[derive(Queryable)]
+#[derive(Debug, Queryable, Deserialize, Serialize)]
 pub struct Post {
     pub id: i32,
     pub title: String,
@@ -12,13 +18,37 @@ pub struct Post {
     pub body: String,
 }
 
-
-use super::schema::posts;
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct NewPostHandler {
+    pub title: String,
+    pub body: String,
+}
 
 #[derive(Insertable)]
-#[table_name="posts"]
-pub struct NewPost<'a>{
+#[diesel(table_name = posts)]
+pub struct NewPost<'a> {
     pub title: &'a str,
     pub body: &'a str,
-    pub slug: &'a str
+    pub slug: &'a str,
+}
+
+impl Post {
+    pub fn slugify(title: &String) -> String {
+        return title.replace(" ", "_").to_lowercase();
+    }
+
+    pub fn create_post<'a>(
+        conn: &mut PgConnection,
+        post: &NewPostHandler
+    ) -> Result<Post, diesel::result::Error> {
+        let slug = Post::slugify(&post.title.clone());
+
+        let new_post = NewPost {
+            title: &post.title,
+            body: &post.body,
+            slug: &slug,
+        };
+
+        diesel::insert_into(posts::table).values(new_post).get_result::<Post>(conn)
+    }
 }
